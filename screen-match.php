7999d1709438a12067d90b56a64f0cdcf5b32c2d
analuisadev <analuisavidal89@gmail.com>
2024-09-23 21:03:10 -0300

    feat: Match

--- screen-match.php
+++ screen-match.php
@@ -16,5 +16,14 @@ echo "Ano de lançamento " . $releaseYear . "\n";
 if ($releaseYear > 2024) {
     echo "Esse filme é um lançamento";
 } else {
-    echo "Esse filme não é um lançamento";
+    echo "Esse filme não é um lançamento \n";
 }
+
+$classification = match($filmName) {
+    "Donzela" => "Ficção Científica",
+    "O Mundo Depois de Nós" => "Ficção Científica",
+    "M3GAN" => "Terror",
+    default => "Gênero desconhecido."
+};
+
+echo "O gênero do filme é: " . $classification;
